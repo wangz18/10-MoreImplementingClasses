6febfb7531b30f72216045611cb7a71201ee41fe
wangz18 <wangz18@rose-hulman.edu>
2018-03-26 12:06:57 -0400

    m1

--- src/m1_Line.py
+++ src/m1_Line.py
@@ -716,7 +716,7 @@ class Line(object):
             print(line2)  # Should print: Line[(0, 1), (10, 20)]
         """
         # --------------------------------------------------------------
-        # TODO: 13.
+        # DONE: 13.
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
